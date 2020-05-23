b9a4b89ffb936e844951d116280ad5bcc5f769df
FernandoGOT <fernandogot@Fernandos-MBP.home>
2020-05-23 06:08:23 -0300

    Initial version

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import {
+  Nav,
+  Navbar,
+  NavLink,
+  NavItem,
+  Collapse,
+  NavbarBrand,
+  NavbarToggler
+} from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const toggle = () => {
+    setOpen(!open)
+  }
+
+  return (
+    <Navbar color='light' light expand='md'>
+      <div className='container'>
+        <NavbarBrand tag={Link} to ='/'>Minhas séries</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={open} navbar>
+            <Nav className='ml-auto' navbar>
+            <NavItem>
+                <NavLink tag={Link} to='/series'>Séries</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink tag={Link} to='/generos'>Genêros</NavLink>
+            </NavItem>
+            </Nav>
+        </Collapse>
+      </div>
+    </Navbar>
+  )
+}
+
+export default Header
